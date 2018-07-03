0ed1cdb1aeeb6407a2996b628665ccae7d653116
Tobias Böker <tb@softwarepioniere.de>
2018-07-03 15:12:03 +0200

    updated namespaces

--- src/SoftwarePioniere.ReadModel.Services.AzureCosmosDb/DependencyInjectionExtensions.cs
+++ src/SoftwarePioniere.ReadModel.Services.AzureCosmosDb/DependencyInjectionExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using SoftwarePioniere.ReadModel;
+using SoftwarePioniere.ReadModel.Services.AzureCosmosDb;
 
-namespace SoftwarePioniere.ReadModel.Services.AzureCosmosDb
+// ReSharper disable once CheckNamespace
+namespace SoftwarePioniere
 {
     public static class DependencyInjectionExtensions
     {
