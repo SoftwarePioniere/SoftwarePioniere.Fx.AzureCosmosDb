36b83db70ac18201a67718b1001458aa60e0efe8
Tobias Böker <tb@softwarepioniere.de>
2018-11-04 13:27:18 +0100

    no collectionid default value

--- src/SoftwarePioniere.ReadModel.Services.AzureCosmosDb/AzureCosmosDbOptions.cs
+++ src/SoftwarePioniere.ReadModel.Services.AzureCosmosDb/AzureCosmosDbOptions.cs
@@ -2,7 +2,7 @@
 {
     public class AzureCosmosDbOptions : EntityStoreOptionsBase
     {
-        public string CollectionId { get; set; } = "dev";
+        public string CollectionId { get; set; }
 
         public string AuthKey { get; set; } = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
